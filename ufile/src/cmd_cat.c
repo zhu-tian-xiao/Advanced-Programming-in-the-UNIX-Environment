296c9bcf4c1bf04ca1323d611e76eae6dbb8b539
zhutianxiao <zhutianxiao22353x@gmail.com>
2026-09-21 17:53:15 +0800

    cmd_cat cmd_cp

--- ufile/src/cmd_cat.c
+++ ufile/src/cmd_cat.c
@@ -3,46 +3,64 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdbool.h>
 #define BUF_SIZE 4096
+
+int cat_fd(int fd);
 int main(int argc, char *argv[]) {
   if (argc < 2 || strcmp(argv[1], "cat") != 0) {
     exit(EXIT_FAILURE);
   }
 
-  bool status = EXIT_SUCCESS;
+  if (argc == 2) {
+    return cat_fd(STDIN_FILENO);
+  }
+
+  int status = EXIT_SUCCESS;
   for (size_t i = 2; i < argc; i++) {
-    int fd = open(argv[i], O_RDONLY);
+    int fd;
+    if (strcmp(argv[i], "-") == 0) {
+      fd = STDIN_FILENO;
+    } else {
+      fd = open(argv[i], O_RDONLY);
+    }
     if (fd == -1) {
       status = EXIT_FAILURE;
       perror(argv[i]);
       continue;
     }
 
-    char buf[BUF_SIZE];
-    ssize_t n;
-    while ((n = read(fd, buf, BUF_SIZE)) > 0) {
-      size_t remaining = n;
-      char *read_buf = buf;
-      while (remaining > 0) {
-        int m = write(STDOUT_FILENO, read_buf, n);
-        if (m < 0) {
-          perror("write error");
-          exit(EXIT_FAILURE);
-        }
-        remaining -= m;
-        read_buf += m;
-      }
+    if (cat_fd(fd) == EXIT_FAILURE) {
+      status = EXIT_FAILURE;
     }
-    if (n < 0) {
-      perror("read error");
-      exit(EXIT_FAILURE);
-    }
-    if (close(fd) == -1) {
+
+    if (fd != STDIN_FILENO && close(fd) == -1) {
       perror("close error");
-      exit(EXIT_FAILURE);
+      status = EXIT_FAILURE;
     }
   }
 
-  exit(status);
+  return status;
+}
+
+int cat_fd(int fd) {
+  char buf[BUF_SIZE];
+  ssize_t n;
+  while ((n = read(fd, buf, BUF_SIZE)) > 0) {
+    size_t remaining = n;
+    char *read_buf = buf;
+    while (remaining > 0) {
+      ssize_t m = write(STDOUT_FILENO, read_buf, remaining);
+      if (m <= 0) {
+        perror("write error");
+        return EXIT_FAILURE;
+      }
+      remaining -= m;
+      read_buf += m;
+    }
+  }
+  if (n < 0) {
+    perror("read error");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
---
+++ ufile/src/cmd_cp.c
@@ -0,0 +1,8 @@
+       #include <sys/types.h>
+       #include <sys/stat.h>
+       #include <unistd.h>
+
+       #include <fcntl.h>
+int main(void) {
+  open()
+}
